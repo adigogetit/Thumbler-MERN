dd36c44e896e7447e9f602e9486203892cab5ed4
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-02-03 00:55:58 +0530

    late night commit

--- client/src/sections/HeroSection.tsx
+++ client/src/sections/HeroSection.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
         "30 days free trial",
         "Setup in 10 minutes",
     ];
-
+//just to chnage coomit time 
     return (
         <div className="relative flex flex-col items-center justify-center px-4 md:px-16 lg:px-24 xl:px-32">
             <div className="absolute top-30 -z-10 left-1/4 size-72 bg-pink-600 blur-[300px]"></div>
