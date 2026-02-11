70165d4c9c2c6dd3c5abc8d55b23bafe84c6b952
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-02-11 23:26:04 +0530

    generate pages started

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,10 +1,25 @@
-
+import { useState } from "react";
+import { useParams } from "react-router-dom"
+import { IThumbnail } from "../assets/assets";
+import SoftBackdrop from "../components/SoftBackDrop";
 
 const Generate = () => {
-  return(
-    <div>
-      <h1></h1>
-    </div>
+
+  const id = { useParams };
+  const [title, setTitle] = useState('');
+  const [additionalDetails, setAdditionalDetails] = useState('');
+
+  const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <SoftBackdrop />
+      <div>
+
+      </div>
+
+    </>
   )
 }
 
