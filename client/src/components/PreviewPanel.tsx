40eee262177a7e0bd0f9d23ac1ec645ed5a1ce9b
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-02-28 23:37:07 +0530

    added ondownload in imgae preview in preview pannel

--- client/src/components/PreviewPanel.tsx
+++ client/src/components/PreviewPanel.tsx
@@ -11,6 +11,11 @@ const PreviewPanel = ({thumbnail,isLoading,aspectRatio} :
     "9:16":"aspect-[9/16]"
   }as Record<AspectRatio,string>
 
+  const onDownload = () => {
+    if(!thumbnail?.image_url) return;
+    window.open(thumbnail.image_url, "_blank")
+  }
+
   return (
     <div className="relative mx-auto w-full max-w-2xl">
       <div className={`relative overflow-hidden ${aspectClasses[aspectRatio]}`}>
@@ -32,7 +37,7 @@ const PreviewPanel = ({thumbnail,isLoading,aspectRatio} :
             <img src={thumbnail?.image_url} alt={thumbnail.title}  className="h-full w-full object-cover"/>
 
             <div className=" absolute inset-0 flex items-end justify-center bg-black/10 opicity-0 transition-opacity group-hover:opacity-100"> 
-              <button type="button" className="mb-6 flex items-center gap-2 rounded-md bg-white/30 px-4 py-2 text-xs font-medium transition ring-2 ring-white/40 backdrop=blur hover:scale-105 active:scale-95">
+              <button onClick={onDownload} type="button" className="mb-6 flex items-center gap-2 rounded-md bg-white/30 px-4 py-2 text-xs font-medium transition ring-2 ring-white/40 backdrop=blur hover:scale-105 active:scale-95">
                 <DownloadIcon className="size-4"/>
                 Download Thumbnail 
               </button>
