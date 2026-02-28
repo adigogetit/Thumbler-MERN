b65dbb2fb7f8f9361d73ab4ae618971dac79b726
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-02-28 22:47:16 +0530

    added aspectclasses in previewpanel

--- client/src/components/PreviewPanel.tsx
+++ client/src/components/PreviewPanel.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import type { AspectRatio, IThumbnail } from "../assets/assets"
 
-const PreviewPanel = () => {
+const PreviewPanel = ({thumbnail,isLoading,aspectRatio} :
+  {thumbnail:IThumbnail , isLoading:boolean, aspectRatio:AspectRatio}
+) => {
+
+  const aspectClasses={
+    "16:9":"aspect-video",
+    "4:3":"aspect-square",
+    "1:1":"aspect-square"
+  }
+  
   return (
     <div>
       
