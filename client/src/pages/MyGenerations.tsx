91a8707df5a48c2b890985137d7786f5bf65a515
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-02 15:31:29 +0530

    made header section in mygenerations

--- client/src/pages/MyGenerations.tsx
+++ client/src/pages/MyGenerations.tsx
@@ -1,10 +1,21 @@
+import SoftBackdrop from "../components/SoftBackdrop"
 
 
 const MyGenerations = () => {
   return (
-    <div>
-      
+    <>
+    <SoftBackdrop/>
+    <div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:ps32"> 
+      {/* {Header} */}
+      <div className="mb-8">
+        <h1 className="text-2xl font-bold text-zinc-200">My Generations</h1>
+        <p className="text-sm text-zinc-200 mt-1">View and manage all your thumbnails</p>
+
+      </div>
+
     </div>
+      
+    </> 
   )
 }
 
