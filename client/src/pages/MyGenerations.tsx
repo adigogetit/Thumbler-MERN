8f386f8a5d6c2622b860420bbcc44e61c6538376
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-02 16:58:15 +0530

    added grid for thumbnails to show on my generations page

--- client/src/pages/MyGenerations.tsx
+++ client/src/pages/MyGenerations.tsx
@@ -1,62 +1,107 @@
 import { useEffect, useState } from "react"
 import SoftBackdrop from "../components/SoftBackdrop"
 import { dummyThumbnails, type IThumbnail } from "../assets/assets"
+import { useNavigate } from "react-router-dom"
 import { div } from "motion/react-client"
 
 
 const MyGenerations = () => {
 
-  const [thumbnail,setThumbnail] = useState<IThumbnail[]>([])
-  const [loading,setLoading] = useState(false)
+  const navigate = useNavigate();
 
-  const fetchThumbnails = async()=>{
+  const aspectRatioClassMap: Record<String, string> = {
+    "16:9": "aspect-video",
+    "4:3": "aspect-square",
+    "9:16": "aspect-[9/16]"
+  }
+
+  const [thumbnail, setThumbnail] = useState<IThumbnail[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const fetchThumbnails = async () => {
     setThumbnail(dummyThumbnails as unknown as IThumbnail[])
     setLoading(false);
   }
 
-  const handelDownload = (image_url:string)=>{
-    window.open(image_url,'_blank')
-  }  
+  const handelDownload = (image_url: string) => {
+    window.open(image_url, '_blank')
+  }
 
-  const handelDelete = async(id:string)=>{
+  const handelDelete = async (id: string) => {
     console.log(id);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchThumbnails()
-  },[])
+  }, [])
 
   return (
     <>
-    <SoftBackdrop/>
-    <div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:ps32"> 
-      {/* {Header} */}
-      <div className="mb-8">
-        <h1 className="text-2xl font-bold text-zinc-200">My Generations</h1>
-        <p className="text-sm text-zinc-200 mt-1">View and manage all your thumbnails</p>
+      <SoftBackdrop />
+      <div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:ps32">
+        {/* {Header} */}
+        <div className="mb-8">
+          <h1 className="text-2xl font-bold text-zinc-200">My Generations</h1>
+          <p className="text-sm text-zinc-200 mt-1">View and manage all your thumbnails</p>
+
+        </div>
+
+        {/* {Loading} */}
+        {loading && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {Array.from({ length: 6 }).map((_, i) => (
+              <div key={i} className="rounded-2xl bg-white/6 border border-white/10 animate-pluse h-[260px]" />
+            ))}
+          </div>
+        )}
+
+        {/* {Empty state} */}
+        {!loading && thumbnail.length === 0 && (
+          <div className="text-center py-24">
+            <h3 className="text-lg font-semibold text-zinc-200">No Thumbnails yet</h3>
+            <p className="text-sm text-zinc-200 mt-2">Generate your first thumbnail to see it here </p>
+          </div>
+        )}
+
+        {/* {Grid} */}
+        {!loading && thumbnail.length > 0 && (
+          <div className="columns-1 sm:columns-2 lg:columns-3 2xl:columns-4 gap-8">
+            {thumbnail.map((thumb: IThumbnail) => {
+              const aspectClass = aspectRatioClassMap[thumb.aspect_ratio || '16:9'];
+
+              return (
+                <div
+                  key={thumb._id}
+                  onClick={() => navigate(`/generate/${thumb._id}`)}
+                  className="mb-8 group relative cursor-pointer rounded-2xl bg-white/6 border border-white/10 transition shadow-xl break-inside-avoid"
+                >
+                  {/* IMAGE */}
+                  <div className={`relative overflow-hidden rounded-t-2xl ${aspectClass} bg-black`}>
+                    {thumb.image_url ? (
+                      <img
+                        src={thumb.image_url}
+                        alt={thumb.title}
+                        className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
+                      />
+                    ) : (
+                      <div className="w-full h-full flex items-center justify-center text-sm text-zinc-400">
+                        {thumb.isGenerating ? "Generating..." : "No image"}
+                      </div>
+                    )}
+
+                    {thumb.isGenerating &&
+                     <div className="absolute inset-0 bg-black/10 flex items-center justify-center text-sm font-medium text-white">Generating..</div>
+                    }
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        )}
 
       </div>
 
-      {/* {Loading} */}
-      {loading && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {Array.from({length:6}).map((_, i)=>(
-            <div key={i} className="rounded-2xl bg-white/6 border border-white/10 animate-pluse h-[260px]"/>
-          ))}
-        </div>
-      )}
-
-      {/* {Empty state} */}
-      {!loading && thumbnail.length===0 && (
-         <div className="text-center py-24">
-          <h3 className="text-lg font-semibold text-zinc-200">No Thumbnails yet</h3>
-          <p className="text-sm text-zinc-200 mt-2">Generate your first thumbnail to see it here </p>
-         </div>
-      )}
-
-    </div>
-      
-    </> 
+    </>
   )
 }
 
