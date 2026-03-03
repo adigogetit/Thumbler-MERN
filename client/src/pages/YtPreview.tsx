27fd9b7d88ecc743793095756c4b9aa0a46b457f
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-03 19:53:51 +0530

    small changes

--- client/src/pages/YtPreview.tsx
+++ client/src/pages/YtPreview.tsx
@@ -11,7 +11,7 @@ const Ytpreview = () => {
 
   return (
     <div className="fixed inset-0 z-100 bg-black">
-      <iframe srcDoc={new_html} width="100%" height="100% allowFullScreen"></iframe>
+      <iframe srcDoc={new_html} width="100%" height="100%" allowFullScreen></iframe>
     </div>
   )
 }
