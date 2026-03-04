f94e4ee941ac4b2cd8d17fe038b8069b555e574f
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-05 00:07:59 +0530

    updated controllers for login

--- server/controllers/AuthControllers.ts
+++ server/controllers/AuthControllers.ts
@@ -49,28 +49,24 @@ export const loginUser = async (req: Request, res: Response) => {
 
         // find user by email
         const user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: 'User already exists with this email' })
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid eamil aur password' })
         }
-
-        // Encrypt the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Create new user
-        const newUser = new User({ name, email, password: hashedPassword });
-        await newUser.save();
+        const isPasswordMatch = await bcrypt.compare(password, user.password || '');
+        if(!isPasswordMatch) {
+            return res.status(400).json({ message: 'Invalid eamil aur password' })
+        }
 
         // setting user data in session
         req.session.isLoggedIn = true;
-        req.session.userId = newUser._id.toString();
+        req.session.userId = user._id.toString();
 
         return res.json({
-            message: 'User registered successfully',
+            message: 'Login Successful',
             user: {
-                id: newUser._id,
-                name: newUser.name,
-                email: newUser.email,
+                id: user._id,
+                name: user.name,
+                email: user.email,
             }
         })
 
