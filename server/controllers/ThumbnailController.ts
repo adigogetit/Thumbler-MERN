31837165bdd121cb279d46d22863c88425be2993
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-21 23:04:44 +0530

    added gemini-1.5-flash-image model

--- server/controllers/ThumbnailController.ts
+++ server/controllers/ThumbnailController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Thumbnail from '../models/Thumbnail.js';
+import { GenerateContentConfig } from '@google/genai';
 
 export const generateThumbnail = async (req: Request, res: Response) => {
     try {
@@ -12,14 +13,29 @@ export const generateThumbnail = async (req: Request, res: Response) => {
             title,
             prompt_used: user_prompt,
             user_prompt,
-            style, 
+            style,
             aspect_ratio,
             color_scheme,
             text_overlay,
             isGenerating: true
         })
+
+        const model = 'gemini-1.5-flash-image';
+        const generationConfig: GenerateContentConfig = {
+            maxOutputTokens: 32768,
+            temperature: 1,
+            topP: 0.95,
+            responseModalities: ['IMAGE'],
+            imageConfig: {
+                aspectRatio: '16:9',
+                imageSize: '1K'
+            }
+        }
+
+
+
     } catch (error) {
 
-        
+
     }
 }
