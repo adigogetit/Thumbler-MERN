02fa52825c191a74b0df5541a04d11f132da94ed
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-03-27 15:42:14 +0530

    user name and logout option added after the user log in

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
-    const  {isLoggedIn , user , logout} = useAuth();
+    const { isLoggedIn, user, logout } = useAuth();
     const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
 
@@ -18,21 +18,36 @@ export default function Navbar() {
                 transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
             >
                 <Link to='/'>
-                    <img src="/logo.svg" alt="logo" className="h-8.5 w-auto"/>
+                    <img src="/logo.svg" alt="logo" className="h-8.5 w-auto" />
                 </Link>
 
                 <div className="hidden md:flex items-center gap-8 transition duration-500">
-                    <Link to='/' className="hover:text-pink-500 transition">Home</Link> 
-                    <Link to='/generate' className="hover:text-pink-500 transition">Generate</Link> 
-                    { isLoggedIn ? <Link to='/my-generation' className="hover:text-pink-500 transition">My-generation</Link>
-                     : <Link to='#' className="hover:text-pink-500 transition">About</Link> }
-                    <Link to='/#' className="hover:text-pink-500 transition">Contact Us</Link> 
+                    <Link to='/' className="hover:text-pink-500 transition">Home</Link>
+                    <Link to='/generate' className="hover:text-pink-500 transition">Generate</Link>
+                    {isLoggedIn ? <Link to='/my-generation' className="hover:text-pink-500 transition">My-generation</Link>
+                        : <Link to='#' className="hover:text-pink-500 transition">About</Link>}
+                    <Link to='/#' className="hover:text-pink-500 transition">Contact Us</Link>
                 </div>
 
-                <button onClick={()=> navigate("/login")} className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-                    Get Started
-                </button>
-                
+                <div className="flex items-center gap-2">
+                    {isLoggedIn ? (
+                        <div className="relative group">
+                            <button className="rounded-full size-8 bg-white/20 border-2 border-white/10">
+                                {user?.name.charAt(0).toUpperCase()}
+                            </button>
+                            <div className="absolute hidden group-hover:block top-6 right-0 pt-4">
+                                <button onClick={() => logout()} className=" bg-white/20 border-2 border-white/10 px-5 py-1.5 rounded">
+                                    Logout
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <button onClick={() => navigate('/login')} className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                            Get Started
+                        </button>
+                    )}
+                </div>
+
                 <button onClick={() => setIsOpen(true)} className="md:hidden">
                     <MenuIcon size={26} className="active:scale-90 transition" />
                 </button>
@@ -41,11 +56,11 @@ export default function Navbar() {
 
             {/* this is for mobile */}
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                    <Link onClick={() => setIsOpen(false)} to='/'>Home</Link> 
-                    <Link onClick={() => setIsOpen(false)} to='/generate'>Generate</Link> 
-                    <Link onClick={() => setIsOpen(false)} to='/my-generation'>My-generation</Link> 
-                    <Link onClick={() => setIsOpen(false)} to='/#'>Contact</Link> 
-                    <Link onClick={() => setIsOpen(false)} to='/login'>Login</Link> 
+                <Link onClick={() => setIsOpen(false)} to='/'>Home</Link>
+                <Link onClick={() => setIsOpen(false)} to='/generate'>Generate</Link>
+                <Link onClick={() => setIsOpen(false)} to='/my-generation'>My-generation</Link>
+                <Link onClick={() => setIsOpen(false)} to='/#'>Contact</Link>
+                <Link onClick={() => setIsOpen(false)} to='/login'>Login</Link>
 
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
                     <XIcon />
