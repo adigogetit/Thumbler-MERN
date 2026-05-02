bd06c16c00db340bc87f4a37016ec69db99ab7d1
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2026-05-03 00:04:56 +0530

    added google auth routes

--- server/routes/AuthRoutes.ts
+++ server/routes/AuthRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser, verifyUser } from "../controllers/AuthControllers.js";
+import { loginUser, logoutUser, registerUser, verifyUser , googleAuthSuccess} from "../controllers/AuthControllers.js";
 import protect from "../middleware/auth.js";
+import passport from "../configs/passport.js";
 
 const AuthRouter = express.Router();
 
@@ -8,5 +9,10 @@ AuthRouter.post('/register', registerUser);
 AuthRouter.post('/login', loginUser);
 AuthRouter.get('/verify', protect, verifyUser);
 AuthRouter.post('/logout', protect, logoutUser);
+
+// GOOGLE AUTH
+AuthRouter.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+AuthRouter.get("/google/callback",passport.authenticate("google", { failureRedirect: "/login" }),googleAuthSuccess);
 
 export default AuthRouter;
