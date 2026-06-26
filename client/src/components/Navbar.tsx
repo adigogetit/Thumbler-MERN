372a1c20403fdb7d29df907d5375427c4ac8d86f
Aditya Kumar Singh <adityasingh12ka36@gmail.com>
2026-06-26 10:30:00 +0530

    changes in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { MenuIcon, XIcon } from "lucide-react";
+import { MenuIcon, XIcon, LogOut } from "lucide-react";
 import { useState } from "react";
 import { motion } from "motion/react";
 import { Link, useNavigate } from "react-router-dom";
@@ -26,52 +26,55 @@ export default function Navbar() {
                     <Link to='/generate' className="hover:text-pink-500 transition">Generate</Link>
 
                     {isLoggedIn ? <Link to='/my-generation' className="hover:text-pink-500 transition">My-generation</Link>
-                        : <Link to='#' className="hover:text-pink-500 transition">About</Link>}
-
+                        : ""}
                     <Link to='/#' className="hover:text-pink-500 transition">Contact Us</Link>
                 </div>
 
-                <div className="relative group">
-                    {/* Avatar */}
-                    <button className="size-10 rounded-full bg-gradient-to-br from-pink-500 to-red-500 flex items-center justify-center text-white font-semibold shadow-lg shadow-pink-500/30">
-                        {user?.name?.charAt(0).toUpperCase()}
-                    </button>
+                <div className="flex items-center gap-2">
+                    {isLoggedIn ? (
+                        <div className="relative group">
+                            {/* Avatar */}
+                            <button className="size-10 rounded-full bg-gradient-to-br from-pink-500 to-red-500 flex items-center justify-center text-white font-semibold shadow-lg shadow-pink-500/30">
+                                {user?.name?.charAt(0).toUpperCase()}
+                            </button>
 
-                    {/* Dropdown */}
-                    <div
-                        className="absolute right-0 mt-3 w-60 rounded-2xl bg-[#171717] border border-white/10 shadow-2xl opacity-0 invisible -translate-y-2 group-hover:opacity-100 group-hover:visible group-hover:translate-y-0
-                        transition-all duration-200 z-50"
-                    >
-                        {/* Header */}
-                        <div className="px-5 py-4 border-b border-white/10">
-                            <div className="flex items-center gap-3">
-
-                                <div className="size-11 rounded-full bg-gradient-to-br from-pink-500 to-red-500 flex items-center justify-center text-white font-semibold">
-                                    {user?.name?.charAt(0).toUpperCase()}
+                            {/* Dropdown */}
+                            <div className="absolute right-0 mt-3 w-60 rounded-2xl bg-[#171717] border border-white/10 shadow-2xl
+                                            opacity-0 invisible -translate-y-2
+                                            group-hover:opacity-100 group-hover:visible group-hover:translate-y-0
+                                            transition-all duration-200 z-50" >
+                                {/* Header */}
+                                <div className="px-5 py-4 border-b border-white/10">
+                                    {user?.name
+                                        ?.toLowerCase()
+                                        .split(" ")
+                                        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                                        .join(" ")}
                                 </div>
 
-                                <div>
-                                    <h3 className="text-white font-semibold">
-                                        {user?.name
-                                            ?.toLowerCase()
-                                            .split(" ")
-                                            .map((word) =>word.charAt(0).toUpperCase() + word.slice(1))
-                                            .join(" ")}
-                                    </h3>
+                                {/* Logout */}
+                                <div className="p-3">
+                                    <button
+                                        onClick={logout}
+                                        className="w-full flex items-center justify-center gap-2 rounded-xl bg-pink-600 hover:bg-pink-700 py-2.5 text-white font-medium transition"
+                                    >
+                                        <LogOut size={18} />
+                                        Logout
+                                    </button>
                                 </div>
                             </div>
                         </div>
+                    ) : (
+                        <button
+                            onClick={() => navigate("/login")}
+                            className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                            Get Started
+                        </button>
+                    )}
 
-                        {/* Logout */}
-                        <div className="p-3">
-                            <button
-                                onClick={logout}
-                                className="w-full rounded-xl bg-pink-600 hover:bg-pink-700 py-2.5 text-white font-medium transition-all duration-200 active:scale-95"
-                            >
-                                Logout
-                            </button>
-                        </div>
-                    </div>
+                    <button onClick={() => setIsOpen(true)} className="md:hidden">
+                        <MenuIcon size={26} className="active:scale-90 transition" />
+                    </button>
                 </div>
 
             </motion.nav>
@@ -83,7 +86,7 @@ export default function Navbar() {
                 <Link onClick={() => setIsOpen(false)} to='/generate'>Generate</Link>
 
                 {isLoggedIn ? <Link onClick={() => setIsOpen(false)} to='/my-generation'>My-generation</Link>
-                    : <Link onClick={() => setIsOpen(false)} to='#'>About</Link>}
+                    : ""}
 
                 <Link onClick={() => setIsOpen(false)} to='/#'>Contact us</Link>
 
